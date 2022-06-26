827c996965e2142ed5c972c2ed80eb4ad075d6d3
Ansyari Aqshal  Raditya JS <64580325+radityajs28@users.noreply.github.com>
2022-06-26 10:40:22 +0000

    adding home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'home/index'
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+  #resources :users
+  root 'home#index'
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
